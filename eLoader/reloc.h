fa11a40d9ae08eea95ff26483100150a97ec71df
ametric <ametric@ae707b24-f6f8-a420-3b91-9542b9ad9fd8>
2010-05-28 13:12:43 +0000

    - Big cleaning house on P5 branch

--- eLoader/reloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELOADER_RELOC
-#define ELOADER_RELOC
-
-// Relocates PRX sections that need to
-// Returns number of relocated entries
-unsigned int relocate_sections(SceUID elf_file, SceOff start_offset, Elf32_Ehdr *pelf_header, void* reloc_addr);
-
-#endif
